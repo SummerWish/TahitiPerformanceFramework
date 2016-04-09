53cdd38e8522ba7c5e1ea41ad78725d03136bc67
justnick <findnick@163.com>
2016-04-10 00:17:13 +0800

    minor fix for test

--- src/test/java/octoteam/tahiti/performance/QuantizedRecorderTest.java
+++ src/test/java/octoteam/tahiti/performance/QuantizedRecorderTest.java
@@ -12,7 +12,7 @@ public class QuantizedRecorderTest {
     public QuantizedRecorder qRecorder = new QuantizedRecorder("Usage", EnumSet.of(
             QuantizedRecorder.OutputField.AVERAGE,
             QuantizedRecorder.OutputField.MAX,
-            QuantizedRecorder.OutputField.AVERAGE.MIN
+            QuantizedRecorder.OutputField.MIN
     ));
 
     @Test
@@ -25,12 +25,16 @@ public class QuantizedRecorderTest {
         qRecorder.record(10);
         qRecorder.record(100);
         qRecorder.record(70);
-        assertEquals("{average=60.00, min=10.00, max=100.00}",qRecorder.getReport());
+        assertTrue(qRecorder.getReport().contains("average=60.00"));
+        assertTrue(qRecorder.getReport().contains("min=10"));
+        assertTrue(qRecorder.getReport().contains("max=100"));
     }
 
     @Test
     public void testReset(){
         qRecorder.reset();
-        assertEquals("{average=NaN, min=NaN, max=NaN}",qRecorder.getReport());
+        assertTrue(qRecorder.getReport().contains("average=NaN"));
+        assertTrue(qRecorder.getReport().contains("min=NaN"));
+        assertTrue(qRecorder.getReport().contains("max=NaN"));
     }
 }
